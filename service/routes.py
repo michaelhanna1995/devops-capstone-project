f26bbfe5013541e4cb4d647fb8d45fe9e3d7fc80
michael.hanna1995 <michael.morkos95@gmail.com>
2025-04-25 19:54:32 -0400

    Read an account

--- service/routes.py
+++ service/routes.py
@@ -62,7 +62,7 @@ def create_accounts():
 ######################################################################
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
-    
+    """List all existing accounts"""
     app.logger.info("Request to list all existing accounts")
     
     accounts = Account.all()
@@ -76,7 +76,18 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    return account.serialize(), status.HTTP_200_OK
+
 
 
 ######################################################################
