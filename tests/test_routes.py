936ffd120223fd5b72e9df7156cc1f37675a09c8
michael.hanna1995 <michael.morkos95@gmail.com>
2025-04-25 19:10:18 -0400

    List all Accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,11 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+
+    def test_list_accounts(self):
+        """ it should list all exisiting accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
